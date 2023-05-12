7e87aa59c036463afc108657339ae5da7d953c2a
Vraj Patel <96194973+vrajpatel1812@users.noreply.github.com>
2023-05-12 21:18:12 +0530

    Add files via upload

---
+++ MergeLinkedList.java
@@ -0,0 +1,65 @@
+package Leetcode;
+
+public class MergeLinkedList {
+    
+    public class ListNode{
+        int val;
+        ListNode next;
+        ListNode(){}
+        ListNode(int x){
+            val = x;
+            next = null;
+        }
+        ListNode(int x, ListNode next){
+            val = x;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
+        if(list1 == null){
+            return list2;
+        }
+
+        if(list2 == null){
+            return list1;
+        }
+
+        ListNode head;
+        ListNode temp;
+
+        if(list1.val < list2.val){
+            temp = head = new ListNode(list1.val);
+            list1 = list1.next;
+        }else{
+            temp = head = new ListNode(list2.val);
+            list2 = list2.next;
+        }
+
+        while(list1 != null && list2 != null){
+                if(list1.val < list2.val){
+                    temp.next = new ListNode(list1.val);
+                    list1 = list1.next;
+                }else{
+                    temp.next = new ListNode(list2.val);
+                    list2 = list2.next;
+                }
+                temp = temp.next;
+        }
+
+        while(list1 != null){
+            temp.next = new ListNode(list1.val);
+            list1 = list1.next;
+            temp = temp.next;
+        }
+
+        while(list2 != null){
+            temp.next = new ListNode(list2.val);
+            list2 = list2.next;
+            temp = temp.next;
+        }
+
+        return head;
+    }
+
+}
