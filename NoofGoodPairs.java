c7fe7f4a0e8af7a84601e1836e5a031381ee2c22
Vraj Patel <96194973+vrajpatel1812@users.noreply.github.com>
2023-05-22 13:26:20 +0530

    Add files via upload

---
+++ NoofGoodPairs.java
@@ -0,0 +1,23 @@
+package Leetcode;
+
+public class NoofGoodPairs {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1, 1, 3};
+        int ans = numIdenticalPairs(nums);
+        System.out.println(ans);
+    }
+
+    public static int numIdenticalPairs(int[] nums){
+        int ans = 0;
+
+        for(int i = 0; i<nums.length; i++){
+            for(int j = i+1; j<nums.length; j++){
+                if(nums[i] == nums[j]){
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
