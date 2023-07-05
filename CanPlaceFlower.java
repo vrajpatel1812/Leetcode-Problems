14f349be23532ad0492c53898960ab0f1facfa2d
Vraj Patel <96194973+vrajpatel1812@users.noreply.github.com>
2023-07-05 22:30:32 +0530

    Add files via upload

---
+++ CanPlaceFlower.java
@@ -0,0 +1,21 @@
+public class CanPlaceFlower {
+    public static void main(String[] args) {
+        
+    }
+
+    public static boolean canPlaceFlowers(int[] flowerbed, int n){
+        if(n == 0){
+            return true;
+        }
+
+        for(int i = 0; i<flowerbed.length; i++){
+            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length-1 || flowerbed[i+1] == 0)){
+                flowerbed[i] = 1;
+                if(--n == 0){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
